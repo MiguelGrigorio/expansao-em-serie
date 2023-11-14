196da37c96f5d2112b9a1b8c3e336b7729fe8cb7
Miguel Grigorio <142750268+MiguelGrigorio@users.noreply.github.com>
2023-11-14 10:42:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ Py_5 = Py["5"]
 Py_10 = Py["10"]
 Py_30 = Py["30"]
 # Diminuir quantidade de pontos
-for i in range(1):
+for _ in range(0):
     del Px[-1]
     del Py_5[-1]
     del Py_10[-1]
@@ -41,15 +41,14 @@ org =   [
             np.subtract(np.power(np.add(1, Px), -2),
                         np.power(np.subtract(1, Px), -2)
                         ),
-            np.subtract(np.add(np.power(np.add(1, Px), -2),
-                               np.power(np.subtract(1, Px), -2)
-                              ),
-                        np.multiply(2, np.power(1, -2))
+            np.subtract(np.add(
+                               np.power(np.add(1, Px), -2), np.power(np.subtract(1, Px), -2)),
+                               2
                        )
         ]
 
-plt.plot(Px, Py_5, label = "n = 5", linestyle = " ", marker = 'v') 
-plt.plot(Px, Py_10, label = "n = 10", linestyle = " ", marker = '^') 
+#plt.plot(Px, Py_5, label = "n = 5", linestyle = " ", marker = 'v') 
+#plt.plot(Px, Py_10, label = "n = 10", linestyle = " ", marker = '^') 
 plt.plot(Px, Py_30, label = "n = 30", linestyle = " ", marker = '<') 
 plt.plot(Px, org[eq], label = orgN[eq], linestyle=" ", marker = '>')
 plt.legend()
